6463bf560c31bf8c244f0e18cc9f534d41d4b0a4
Milos Damjanovic <milos.damjanovic064@gmail.com>
2017-05-17 16:46:19 +0200

    Nas prvi commit - 3.lekcija

---
+++ app/src/main/java/com/ana/marija/milos/traceyoursteps/MainActivity.java
@@ -0,0 +1,27 @@
+package com.ana.marija.milos.traceyoursteps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static String className;
+
+    public MainActivity(){
+        className=getClass().getName();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView zdravo = (TextView) findViewById(R.id.hello);
+        Log.d(className, "Settting text");
+
+        zdravo.setText("Zdravo svete");
+
+    }
+}
